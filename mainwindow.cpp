806695eff1ad3c338cb9f94fe7dae49177f9a3bf
thomas.winckell <thomas.winckell@gmail.com>
2014-03-08 20:37:22 +0000

    wizard

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,26 +14,17 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->radioButtonToutesPhotos->setChecked(true);
-
     mCollage.setListPhoto(ui->widgetPhotos->getListPhotos());
 
     QPixmap imageApercu(ui->labelApercu->width(), ui->labelApercu->height());
     ui->labelApercu->setPixmap(imageApercu);
 
     connect(&mCollage, SIGNAL(valueChanged(int)), ui->progressBarApercu, SLOT(setValue(int)));
-
-    mUIntValidator = new QIntValidator(0, std::numeric_limits<int>::max(), ui->lineEditLargeurCollage->parent());
-    ui->lineEditLargeurCollage->setValidator(mUIntValidator);
-    ui->lineEditHauteurCollage->setValidator(mUIntValidator);
-    ui->lineEditTaillePhoto->setValidator(mUIntValidator);
-    ui->lineEditNbPhotos->setValidator(mUIntValidator);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    delete mUIntValidator;
 }
 
 void MainWindow::on_actionQuitter_triggered()
@@ -57,58 +48,20 @@ void MainWindow::on_pushButtonApercu_clicked()
 
 void MainWindow::majCollage()
 {
-    int nbPhotos;
-    int w = ui->lineEditLargeurCollage->text().toInt();
-    int h = ui->lineEditHauteurCollage->text().toInt();
+    int w = ui->widgetSizes->getCollageWidth();
+    int h = ui->widgetSizes->getCollageHeight();
 
-    QPolygon polygon;
-
-    ui->radioButtonToutesPhotos->isChecked() ? nbPhotos = ui->widgetPhotos->countListPhotos() : nbPhotos = ui->lineEditNbPhotos->text().toInt();
-
-    if (ui->radioButtonPolygone->isChecked())
-        polygon = dialogPolygon.mPolygon;
-    else
-    {
-        if (ui->radioButtonRectangle->isChecked())
-        {
-            polygon.push_back(QPoint(0,0));
-            polygon.push_back(QPoint(0,20));
-            polygon.push_back(QPoint(30, 20));
-            polygon.push_back(QPoint(30,0));
-        }
-        else
-        {
-            int nbPoints = 36;
-            double slice = 2 * (atan(1.0)*4) /nbPoints;
-            double rad, px, py;
-            int rayon;
-
-            if(h > w)
-                rayon = w / 2;
-            else
-                rayon = h / 2;
-
-            for (int i = 0; i < nbPoints; i++)
-            {
-                rad = slice * i;
-                px = 200 + rayon * cos(rad);
-                py = 120 + rayon * sin(rad);
-                polygon.push_back(QPoint(px,py));
-            }
-        }
-    }
-
-    mCollage.setPolygon(polygon);
+    mCollage.setPolygon(ui->widgetShape->getPolygon(w, h));
     mCollage.setWidth(w);
     mCollage.setHeight(h);
-    mCollage.setPhotoSize(ui->lineEditTaillePhoto->text().toInt());
-    mCollage.setDistancePhotos(ui->spinBoxDistancePhotos->value());
-    mCollage.setNbPhotos(nbPhotos);
-    mCollage.setAngleMax(ui->sliderAngle->value());
-    mCollage.setAutoSize(ui->pushButtonAutoManuelTailleCollage->isChecked());
-    mCollage.setAutoPhotoSize(ui->pushButtonAutoManuelTaillePhoto->isChecked());
-    mCollage.setAutoNbPhotos(ui->pushButtonAutoManuelNbPhoto->isChecked());
-    mCollage.setAutoDistancePhotos(ui->pushButtonAutoManuelDistancePhoto->isChecked());
+    mCollage.setPhotoSize(ui->widgetSizes->getPhotoSize());
+    mCollage.setDistancePhotos(ui->widgetSizes->getDistancePhotos());
+    mCollage.setNbPhotos(ui->widgetSizes->getNbPhotos(ui->widgetPhotos->countListPhotos()));
+    mCollage.setAngleMax(ui->widgetRotationMax->getValue());
+    mCollage.setAutoSize(ui->widgetSizes->getAutoSize());
+    mCollage.setAutoPhotoSize(ui->widgetSizes->getAutoPhotoSize());
+    mCollage.setAutoNbPhotos(ui->widgetSizes->getAutoNbPhotos());
+    mCollage.setAutoDistancePhotos(ui->widgetSizes->getDistancePhotos());
 }
 
 void MainWindow::renderApercu()
@@ -124,149 +77,18 @@ void MainWindow::clearApercu()
     ui->labelApercu->setPixmap(imageApercu);
 }
 
-void MainWindow::on_radioButtonPolygone_clicked()
-{
-    dialogPolygon.show();
-}
-
-void MainWindow::majTaille()
-{
-    bool enabled;
-
-    /* màj taille collage */
-    if (ui->pushButtonAutoManuelTailleCollage->isChecked())
-    {
-        ui->pushButtonAutoManuelTailleCollage->setText("Auto");
-        enabled = false;
-    }
-    else
-    {
-        ui->pushButtonAutoManuelTailleCollage->setText("Manuel");
-        enabled = true;
-    }
-
-    ui->lineEditHauteurCollage->setEnabled(enabled);
-    ui->lineEditLargeurCollage->setEnabled(enabled);
-    ui->labelPixelsCollage->setEnabled(enabled);
-    ui->labelHauteur->setEnabled(enabled);
-    ui->labelLargeur->setEnabled(enabled);
-    ui->labelX->setEnabled(enabled);
-
-    /* màj taille photo */
-    if (ui->pushButtonAutoManuelTaillePhoto->isChecked())
-    {
-        ui->pushButtonAutoManuelTaillePhoto->setText("Auto");
-        enabled = false;
-    }
-    else
-    {
-        ui->pushButtonAutoManuelTaillePhoto->setText("Manuel");
-        enabled = true;
-    }
-
-    ui->lineEditTaillePhoto->setEnabled(enabled);
-    ui->labelPixelsPhoto->setEnabled(enabled);
-
-    /* màj nombre photos */
-    if (ui->pushButtonAutoManuelNbPhoto->isChecked())
-    {
-        ui->pushButtonAutoManuelNbPhoto->setText("Auto");
-        enabled = false;
-    }
-    else
-    {
-        ui->pushButtonAutoManuelNbPhoto->setText("Manuel");
-        enabled = true;
-    }
-
-    ui->lineEditNbPhotos->setEnabled(enabled);
-    ui->labelPhotos->setEnabled(enabled);
-    ui->radioButtonToutesPhotos->setEnabled(enabled);
-    ui->radioButtonNbPhotos->setEnabled(true);
-
-    /* màj distance photos */
-    if (ui->pushButtonAutoManuelDistancePhoto->isChecked())
-    {
-        ui->pushButtonAutoManuelDistancePhoto->setText("Auto");
-        enabled = false;
-    }
-    else
-    {
-        ui->pushButtonAutoManuelDistancePhoto->setText("Manuel");
-        enabled = true;
-    }
-
-    ui->spinBoxDistancePhotos->setEnabled(enabled);
-    ui->labelPourcent->setEnabled(enabled);
-    ui->horizontalSliderDistancePhotos->setEnabled(enabled);
-}
-
-void MainWindow::on_pushButtonAutoManuelTailleCollage_clicked()
-{
-    majTaille();
-}
-
-void MainWindow::on_pushButtonAutoManuelTaillePhoto_clicked()
-{
-    majTaille();
-}
-
-void MainWindow::on_pushButtonAutoManuelNbPhoto_clicked()
-{
-    majTaille();
-}
-
-void MainWindow::on_pushButtonAutoManuelDistancePhoto_clicked()
-{
-    majTaille();
-}
-
-void MainWindow::on_radioButtonToutesPhotos_clicked()
-{
-    ui->radioButtonToutesPhotos->setChecked(true);
-    ui->radioButtonNbPhotos->setChecked(false);
-}
-
-void MainWindow::on_radioButtonNbPhotos_clicked()
-{
-    ui->radioButtonToutesPhotos->setChecked(false);
-    ui->radioButtonNbPhotos->setChecked(true);
-}
-
-void MainWindow::on_horizontalSliderDistancePhotos_valueChanged(int value)
-{
-    ui->spinBoxDistancePhotos->setValue(value);
-}
-
-void MainWindow::on_spinBoxDistancePhotos_valueChanged(int arg1)
-{
-    ui->horizontalSliderDistancePhotos->setValue(arg1);
-}
-
 void MainWindow::on_pushButtonReinitFormeTaille_clicked()
 {
     reinitUI();
 }
 
-void MainWindow::reinitUI() {
-    ui->radioButtonRectangle->setChecked(true);
+void MainWindow::reinitUI()
+{
+    ui->widgetShape->reinitUI();
 
-    ui->pushButtonAutoManuelTailleCollage->setChecked(true);
-    majTaille();
+    ui->widgetSizes->reinitUI();
 
-    ui->lineEditLargeurCollage->setText("1680");
-    ui->lineEditHauteurCollage->setText("1050");
-
-    ui->lineEditTaillePhoto->setText("200");
-
-    ui->radioButtonToutesPhotos->setChecked(true);
-    ui->radioButtonNbPhotos->setChecked(false);
-    ui->lineEditNbPhotos->setText("150");
-
-    ui->spinBoxDistancePhotos->setValue(100);
-    ui->horizontalSliderDistancePhotos->setValue(100);
-
-    ui->sliderAngle->setValue(60);
+    ui->widgetRotationMax->reinitUI();
 
     ui->progressBarApercu->setValue(0);
 }
@@ -297,11 +119,6 @@ void MainWindow::createCollage()
         QMessageBox::critical(this, "Erreur",
                                         "Il n'y a aucune photos chargée.\nVeuillez charger des photos puis ré-essayer.");
     }
-}
-
-void MainWindow::on_sliderAngle_valueChanged(int value)
-{
-    ui->labelAngle->setText(QString::number(value) + "°");
 }
 
 void MainWindow::on_actionNouveau_projet_triggered()
